586abb1c238eb95aa703f6a64987078f2408d8a6
pressfswearing <145866484+pressfswearing@users.noreply.github.com>
2023-11-07 12:52:09 +0300

    Delete dnevniklib-main directory

--- dnevniklib-main/dnevniklib/school/school.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-from dnevniklib.student import Student
-
-
-class School:
-    def __init__(self, student: Student):
-        self.token = student.token
--- dnevniklib-main/dnevniklib/types/__init__.py
+++
@@ -1,1 +0,0 @@
-from .homework import Homework
